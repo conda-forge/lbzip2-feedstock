939b7854a5357805c37733e966c5beae8cf3c053
Filipe Fernandes <ocefpaf@gmail.com>
2019-04-14 10:00:14 -0300

    remove options from AR

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 mkdir build && pushd build
 
-CFLAGS="${CFLAGS} -gllvm -O0" ${SRC_DIR}/configure --prefix=${PREFIX}
+CFLAGS="${CFLAGS}" ${SRC_DIR}/configure --prefix=${PREFIX}
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check ${VERBOSE_AT}
