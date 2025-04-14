ec5a048e0543cdc59d992e060a440f57665ea58c
David Lecomber <david.lecomber@arm.com>
2025-04-14 12:42:49 +0100

    New build script, also generates configure and other files

---
+++ recipe/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# gnulib-tool is needed to generate the ./configure - it is supposed to be picked up as source
+# and included in applications:
+#
+# From: https://www.gnu.org/software/gnulib/
+#
+#   Gnulib is a central location for common GNU code, intended to be shared among GNU packages.
+#   GCC has libiberty, but this is hard to disentangle from the GCC build tree.
+#
+#   Gnulib takes a different approach. Its components are intended to be shared at the source level,
+#   rather than being a library that gets built, installed, and linked against. Thus, there is no
+#   distribution tarball; the idea is to copy files from Gnulib into your own source tree.
+
+wget https://github.com/coreutils/gnulib/archive/refs/tags/v1.0.tar.gz
+tar xvf v1.0.tar.gz
+
+export PATH=${PATH}:${PWD}/gnulib-1.0
+
+build-aux/autogen.sh
+./configure --prefix=$PREFIX
+make -j${CPU_COUNT}
+if [ "${build_platform}" = "${target_platform}" ]; then
+    make check         # [build_platform == target_platform]
+fi
+make install
