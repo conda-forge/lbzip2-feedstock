47a8e69d5d667f6b29d8bfe934189a45e69fe93c
David Lecomber <david.lecomber@arm.com>
2025-04-17 10:18:19 +0100

    move comments to meta.yaml

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 
-# gnulib-tool is needed to generate the ./configure - it is supposed to be picked up as source
-# and included in applications:
-#
-# From: https://www.gnu.org/software/gnulib/
-#
-#   Gnulib is a central location for common GNU code, intended to be shared among GNU packages.
-#   GCC has libiberty, but this is hard to disentangle from the GCC build tree.
-#
-#   Gnulib takes a different approach. Its components are intended to be shared at the source level,
-#   rather than being a library that gets built, installed, and linked against. Thus, there is no
-#   distribution tarball; the idea is to copy files from Gnulib into your own source tree.
-
+# add meta.yaml downloaded gnulib-tool to the path to generate the
+# necessary autoconf content for autogen.sh
 
 export PATH=${PATH}:${PWD}/gnulib
 
 build-aux/autogen.sh
+
 ./configure --prefix=$PREFIX
 make -j${CPU_COUNT} 
 make check
