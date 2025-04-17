58e39948028a97bc310f6a5324e16fd2c060e003
David Lecomber <david.lecomber@arm.com>
2025-04-17 10:07:20 +0100

    Try make check without filtering on cross-compiler/emulation mode

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,7 +18,5 @@ export PATH=${PATH}:${PWD}/gnulib
 build-aux/autogen.sh
 ./configure --prefix=$PREFIX
 make -j${CPU_COUNT} 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-    make check
-fi
+make check
 make install
